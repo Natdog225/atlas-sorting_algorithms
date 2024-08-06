696bc7b7adbeb7a75bf900db3da873b464feffff
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-05 22:26:34 -0500

    Update to main.h

--- sort.h
+++ sort.h
@@ -22,5 +22,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size);
+void quick_sort_recursive(int *array, ssize_t low, ssize_t high, size_t size);
+void quick_sort(int *array, size_t size);
+
+
 
 #endif
