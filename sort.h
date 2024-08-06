1e15e01961846a152eff172c812350dd42eb59b2
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-08-05 22:24:37 -0500

    Added prototype

--- sort.h
+++ sort.h
@@ -21,5 +21,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size);
 
 #endif
